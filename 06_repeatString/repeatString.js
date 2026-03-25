460617a6fb47dc1aaa61821f87289439fdeb6545
Bhavesh Mistry <bhavesh.mistry@razorpay.com>
2026-03-25 11:53:26 +0530

    Refactor repeatString and reverseString functions to include parameters for string and count

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let result = ""
+    for(let num = 1; num <= count; num++) {
+        result += string
+    }
+    return result
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    result = []
+    chars = string.split()
+    chars.forEach((element) => result += element)
+    return result
 };
 
 // Do not edit below this line
